68e929cbdbeb242af2ec5886025e7d98e438f98a
huseynt <huseyn.tapdiqli.i@gmail.com>
2024-05-18 23:49:36 +0400

    _

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ $(document).ready(() => {
                 type: "POST",
                 headers: {
                     'accept': 'application/json',
-                    'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tL2FwaWJyZXcvYXBpYnJldyIsInN1YiI6ImNvbnRyb2xsZXIiLCJhdWQiOlsiZ2l0aHViLmNvbS9hcGlicmV3L2FwaWJyZXciXSwiZXhwIjoxNzIxMjM3ODA0LCJuYmYiOjE3MTYwNTM4MDQsImlhdCI6MTcxNjA1MzgwNCwianRpIjoiMTdmMWI3NzktNDA0Yy00ZmJkLWFjM2ItOTRlYjk3ODAxZjhkIiwidXNlcm5hbWUiOiJjb250cm9sbGVyIiwidWlkIjoiOTUzNGU4ODktM2VjYy00ODdmLTk5YTQtMTdkYmYxZTIyYTNhIiwic2NvcGVzIjpbIjo6RlVMTDo6Ojo6OkFMTE9XIl0sInBlcm1pc3Npb25zIjpbeyJpZCI6IjA1ZmMxZWRmLTgyODAtNGJkZC05NzhiLTlmYmJkYWE3OTgxOCIsInZlcnNpb24iOjEsIm9wZXJhdGlvbiI6IkZVTEwiLCJwZXJtaXQiOiJBTExPVyJ9LHsiaWQiOiIwNWZjMWVkZi04MjgwLTRiZGQtOTc4Yi05ZmJiZGFhNzk4MTgiLCJ2ZXJzaW9uIjoxLCJvcGVyYXRpb24iOiJGVUxMIiwicGVybWl0IjoiQUxMT1cifV19.r920itFr5jfSbag-olcNu3bWhcvvexy-JWU1uSZXyAuzlkct8zNPYejigpXmQE9YINcjPVR9c7_VCb_i0wwUc2jHGihGqbn7HpZrycUqJVd6fP4Y_SrnjUT2ZTw3XAHcCn3wDkLod67AfmGr69TSLndxr5zyZHwdUfpxB4w2F0U'
+                    'Authorization': ' API key '
                 },
                 data: JSON.stringify(data)
             })
